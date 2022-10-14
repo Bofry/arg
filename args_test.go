ca5fe6fb580fe30f77bb75d6e75ca17429d68115
Wilhelm Liao <williaml.twcp@gmail.com>
2022-10-14 09:07:29 +0800

    WIP: add test case

--- args_test.go
+++ args_test.go
@@ -5,12 +5,36 @@ import "testing"
 func TestString(t *testing.T) {
 	var (
 		emprtyString string = ""
+		fooString    string = "foo"
 	)
+	{
+		err := String.Assert(emprtyString, "emprtyString",
+			String.NonEmpty,
+			String.In("foo", "bar"),
+		)
+		if err == nil {
+			t.Errorf("should get error")
+		}
+	}
+	{
+		err := String.Assert(fooString, "fooString",
+			String.NonEmpty,
+			String.In("foo", "bar"),
+		)
+		if err != nil {
+			t.Errorf("should no error")
+		}
+	}
 
-	err := String.Assert(emprtyString, "emprtyString",
-		String.NonEmpty,
-	)
-	if err == nil {
-		t.Errorf("should get error")
+	// use Assertor
+	{
+		err := String.Assertor(emprtyString, "emprtyString").
+			Assert(
+				String.NonEmpty,
+				String.In("foo", "bar"),
+			)
+		if err == nil {
+			t.Errorf("should get error")
+		}
 	}
 }
