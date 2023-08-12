4fd284730140ef90a1824102b54e933c572e6bf6
Wilhelm Liao <williaml.twcp@gmail.com>
2023-08-13 01:55:20 +0800

    WIP: formating

--- arg.go
+++ arg.go
@@ -1,39 +1,39 @@
-// Package arg implements functions to validate arguments.
-//
-// Synopsis:
-//  var (
-//  	host     string = "127.0.0.1"
-//  	status   string = "connecting"
-//  	requests int64  = 99
-//  )
-//
-//  err := arg.Assert(
-//  	arg.Strings.Assert(host, "host",
-//  		arg.Strings.NonEmpty,
-//  	),
-//  	arg.Strings.Assert(status, "status",
-//  		arg.Strings.NonEmpty,
-//  		arg.Strings.In("connecting", "closed", "aborted"),
-//  	),
-//  	arg.Ints.Assert(requests, "requrests",
-//  		arg.Ints.NonNegativeInteger,
-//  	),
-//  )
-//  fmt.Println(err)  // Output: <nil>
-//
-// The Strings, Floats, Ints, Numbers, Values also offer `Assertor()`:
-//
-//  err := arg.Strings.Assertor(host, "host").
-//  	Assert(
-//  		arg.Strings.NonEmpty,
-//  	)
-//
-// is preferable to
-//
-//  err := arg.Strings.Assert(host, "host",
-//  	arg.Strings.NonEmpty,
-//  )
-//
+/* Package arg implements functions to validate arguments.
+ *
+ * Synopsis:
+ *  var (
+ *  	host     string = "127.0.0.1"
+ *  	status   string = "connecting"
+ *  	requests int64  = 99
+ *  )
+ *
+ *  err := arg.Assert(
+ *  	arg.Strings.Assert(host, "host",
+ *  		arg.Strings.NonEmpty,
+ *  	),
+ *  	arg.Strings.Assert(status, "status",
+ *  		arg.Strings.NonEmpty,
+ *  		arg.Strings.In("connecting", "closed", "aborted"),
+ *  	),
+ *  	arg.Ints.Assert(requests, "requrests",
+ *  		arg.Ints.NonNegativeInteger,
+ *  	),
+ *  )
+ *  fmt.Println(err)  // Output: <nil>
+ *
+ * The Strings, Floats, Ints, Numbers, Values also offer `Assertor()`:
+ *
+ *  err := arg.Strings.Assertor(host, "host").
+ *  	Assert(
+ *  		arg.Strings.NonEmpty,
+ *  	)
+ *
+ * is preferable to
+ *
+ *  err := arg.Strings.Assert(host, "host",
+ *  	arg.Strings.NonEmpty,
+ *  )
+ */
 package arg
 
 func Assert(errs ...error) error {
